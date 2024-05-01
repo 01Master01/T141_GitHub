0d2a54e1906348335cea52d5573c4aa034a14fa4
Meltem Avluk <ml@gmail.com>
2024-05-01 16:46:51 +0300

    second version

---
+++ src/P03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P03 {
+    public static void main(String[] args) {
+
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Lutfen adinizi giriniz");
+        String isim=scn.nextLine();
+
+    }
+}
